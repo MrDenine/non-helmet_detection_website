37c852d95033af67c366d8997ecb99fed263739b
NineTongnois <panuwat_nine@hotmail.com>
2021-11-11 02:59:32 +0700

    add login

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 //Imports
 const express = require('express');
 const axios = require('axios');
+const config = require('./config');
 const app = express();
 const port = 8000;
-
-//Define Routing Path
-var indexRouter = require('./routes/indexRoute');
-var aboutRouter = require('./routes/aboutRouter');
 
 //Static File
 app.use(express.static('public'));
@@ -17,10 +14,26 @@ app.use('/images', express.static(__dirname + 'public/images'));
 //Static Node_Module File
 app.use('/plugins', express.static(__dirname + '/node_modules'));
 
-//call service
-axios.get('http://localhost:3000/')
+//connection service validate 
+axios.get(config.ServiceUrl)
     .then(function (res){
+        //log result
         console.log('\x1b[36m%s\x1b[0m','[API] '+ res.data.data +' :D');
+
+        //Define Routing Path
+        var indexRouter = require('./routes/indexRoute');
+        var loginRouter = require('./routes/loginRouter');
+
+        //Static Routes File
+        app.use('/',indexRouter);
+        app.use('/login',loginRouter);
+
+        //Set Views engine
+        app.set('views', './views');
+        app.set('view engine' , 'ejs');
+
+        //Listen on port 3000
+        app.listen(port,()=> console.info(`[SERVER] Listening on port ${port}`));
     })
     .catch(function (err){
         app.get('/',function(req,res){
@@ -30,16 +43,5 @@ axios.get('http://localhost:3000/')
         console.log('\x1b[31m%s\x1b[0m', '[API] Please enable service before start on port!!!');
         process.exit(1);
     });
-
-//Set Views engine
-app.set('views', './views');
-app.set('view engine' , 'ejs');
-
-//Static Routes File
-app.use('/',indexRouter);
-app.use('/about',aboutRouter);
-
-//Listen on port 3000
-app.listen(port,()=> console.info(`[SERVER] Listening on port ${port}`));
 
 module.exports = app;
---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {};
+
+config.ServiceUrl = 'http://localhost:3000';
+
+module.exports = config;
