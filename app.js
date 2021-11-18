55484a0aa59a294876ec8d8c92f930fc73d2b47e
NineTongnois <panuwat_nine@hotmail.com>
2021-11-19 02:02:36 +0700

    init

--- app.js
+++ app.js
@@ -5,43 +5,46 @@ const config = require('./config');
 const app = express();
 const port = 8000;
 
-//Static File
-app.use(express.static('public'));
-app.use('/stylesheets', express.static(__dirname + 'public/stylesheets'));
-app.use('/javascripts', express.static(__dirname + 'public/javascripts'));
-app.use('/images', express.static(__dirname + 'public/images'));
+try{
+    //Static File
+    app.use(express.static('public'));
+    app.use('/stylesheets', express.static(__dirname + 'public/stylesheets'));
+    app.use('/javascripts', express.static(__dirname + 'public/javascripts'));
+    app.use('/images', express.static(__dirname + 'public/images'));
 
-//Static Node_Module File
-app.use('/plugins', express.static(__dirname + '/node_modules'));
+    //Static Node_Module File
+    app.use('/plugins', express.static(__dirname + '/node_modules'));
 
-//connection service validate 
-axios.get(config.ServiceUrl)
-    .then(function (res){
-        //log result
-        console.log('\x1b[36m%s\x1b[0m','[API] '+ res.data.data +' :D');
+    //connection service validate 
+    axios.get(config.ServiceUrl)
+        .then(function (res){
+            //log result
+            console.log('\x1b[36m%s\x1b[0m','[API] '+ res.data.data +' :D');
 
-        //Define Routing Path
-        var indexRouter = require('./routes/indexRoute');
-        var loginRouter = require('./routes/loginRouter');
+            //Define Routing Path
+            var indexRouter = require('./routes/indexRoute');
 
-        //Static Routes File
-        app.use('/',indexRouter);
-        app.use('/login',loginRouter);
+            //Static Routes File
+            app.use('/',indexRouter);
 
-        //Set Views engine
-        app.set('views', './views');
-        app.set('view engine' , 'ejs');
+            //Set Views engine
+            app.set('views', './views');
+            app.set('view engine' , 'ejs');
 
-        //Listen on port 3000
-        app.listen(port,()=> console.info(`[SERVER] Listening on port ${port}`));
-    })
-    .catch(function (err){
-        app.get('/',function(req,res){
-            res.status(404);
+            //Listen on port 3000
+            app.listen(port,()=> console.info(`[SERVER] Listening on port ${port}`));
         })
-        console.log('\x1b[31m%s\x1b[0m', '[API] Unable to contact API...');
-        console.log('\x1b[31m%s\x1b[0m', '[API] Please enable service before start on port!!!');
-        process.exit(1);
-    });
+        .catch(function (err){
+            app.get('/',function(req,res){
+                res.status(404);
+            })
+            console.log(err);
+            // console.log('\x1b[31m%s\x1b[0m', '[API] Unable to contact API...');
+            // console.log('\x1b[31m%s\x1b[0m', '[API] Please enable service before start on port!!!');
+            process.exit(1);
+        });
 
-module.exports = app;
+    module.exports = app;
+}catch (e){
+    console.log('\x1b[36m%s\x1b[0m','Exception :'+ e)
+}
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,8 +3,24 @@ const router = express.Router();
 
 /* GET Default Page*/
 router.get('/',function(req,res,next){
-    res.render('index');
+    res.render('index',{result : 'GG'});
 });
 
+router.post('/auth',function(req,res,next){
+    var username = req.body.username;
+    var password = req.body.password;
+    if(username && password){
+        axios.post(config.ServiceUrl + '/Login/PostLogin',{
+            email : username,
+            password : password,
+        })
+        .then(function (response) {
+            res.render('login',{result : response.data.data});
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+});
 
 module.exports = router;
